6ef199c56fc5476eb861e6d09714c8290ad5699d
cpearce-py <corin@pearce-web.co.uk>
2021-04-06 16:52:24 -0400

    Added Pawn Promote logic, defaults to Queen.

--- Pieces/Pawn.py
+++ Pieces/Pawn.py
@@ -1,14 +1,42 @@
 import logic
 from AbstractPiece import AbstractPiece
-from Files import Color, Files, IMAGES
+from Files import Color, Files, IMAGES, RANKS
 from Location import Location
-
+from Pieces import Queen
 
 class Pawn(AbstractPiece):
 
     def __init__(self, pieceColor, name="Pawn"):
         img = IMAGES['bp'] if pieceColor == Color.DARK else IMAGES['wp']
         super().__init__(name, pieceColor, image=img)
+
+    def moveToSquare(self, square, moves, board=None):
+        if not moves:
+            raise ValueError("No possible moves!")
+
+        if square.location in moves:
+
+            # Check for promotion
+            if square.location.rank in [1, 8]:
+                self.promote()
+
+            self.square.reset()
+            self.square = square
+            square.currentPiece = self
+            self.isFirstMove = False
+            self.rect.center = square.rect.center
+
+        else:
+            raise ValueError("Piece cannont move to that square.")
+
+    def promote(self):
+        print("Promoting from method")
+        square = self.square
+        self.__class__ = Queen
+        self.__init__(self.color)
+        square.reset()
+        self.square = square
+
 
     def getValidMoves(self, board):
 
